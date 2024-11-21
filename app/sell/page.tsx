111a9f83c403553de69ae3763e239141512b5c81
Mohammad Atir <77569073+Atir-786@users.noreply.github.com>
2024-11-22 01:50:05 +0530

    protected some routes

--- app/sell/page.tsx
+++ app/sell/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { AddPropertyForm } from "@/components/client/form";
-function Sell() {
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+async function Sell() {
+  const session = await auth();
+  const user = session?.user;
+  if (!user) redirect("/login");
   return (
     <div className="text-4xl">
       <AddPropertyForm />
